5cd38d4152a7cde927b36587fc36ce6b5282b6b8
Andrew Baines <andrew.baines@argodata.com>
2018-03-27 16:13:55 -0500

    Todo app.

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+//import Calculator from "./Calculator";
+import Simple from "./Simple";
+
+class App extends Component {
+
+    render() {
+        return (
+            <div className="App">
+                <Simple/>
+            </div>
+        );
+    }
+}
+
+export default App;
